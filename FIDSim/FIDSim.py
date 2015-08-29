05c590e713060424fe223e1997030794728aad4b
Jamie Graham <jamiegraham93@gmail.com>
2015-08-29 18:57:33 +0100

    Updated simulation output method to np.savetxt, and input to ZC chenged to np.loadtxt

---
+++ FIDSim/FIDSim.py
@@ -0,0 +1,29 @@
+import numpy as np
+import random
+import sys
+import matplotlib.pyplot as plt
+
+# This code is run using the following input format:
+# < python FIDSim.py [Number of points] [Frequency] [Timestep] [SNR (dB)] [Phase Offset (radians)]>
+
+# Take inputs:
+NumPoints = int(sys.argv[1])
+Freq = float(sys.argv[2])
+Timestep = float(sys.argv[3])
+SNR = float(sys.argv[4])
+PhaseOffset = float(sys.argv[5])
+
+# Calculate the variance from the given SNR value in order to set the width of the Gaussian noise to be added
+Variance = np.sqrt(1.0/(2.0*SNR))
+
+# Prepare return lists
+Sim_FID_Data = []
+Time = []
+
+# For each required data point, calculate the sinusoid, add noise and calculate the time. Store these in their respective arrays
+for sample in range(NumPoints):
+    Sim_FID_Data.append(np.sin(2.0*np.pi*Freq*Timestep*sample + PhaseOffset) + np.random.normal(0,Variance))
+    Time.append(sample*Timestep)
+
+# Output
+print([Time,Sim_FID_Data])
---
+++ ZCAnalysis/ZC.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# Analysis code that takes an input .csv, .txt, or .dat file in the format (using "#" as a comment character):
+# #Time  Voltage
+# 0.0   0.012
+# 0.1   0.232
+#  .     .
+#  .     .
+#  .     .
+#
+# Function is called using:
+# < python ZC.py [Filename] [Threshold]>
+# where Filename is the name of the input file, e.g. "DataFile001.txt"
+#       Threshold is hysteresis level the state machine uses to filter out small deviations, c.f. Schmidt Filters
+
+# Take input data and type it
+filename = str(sys.argv[1])
+thresh = float(sys.argv[2])
+
+# Set up a state boolean such that True corresponds to the voltage being positive, and False corresponds to the voltage being negative
+Data_State = True
+
+# Load the data file
+numpy.loadtxt(filename)
+# Crossing count initialization
