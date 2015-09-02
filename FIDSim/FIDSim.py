944422e2e616e3ee820ff35601de07c3c0d48101
Jamie Graham <jamiegraham93@gmail.com>
2015-09-02 01:00:34 +0100

    Added decay functionality to FIDSim

--- FIDSim/FIDSim.py
+++ FIDSim/FIDSim.py
@@ -4,15 +4,16 @@ import sys
 import matplotlib.pyplot as plt
 
 # This code is run using the following input format:
-# < python FIDSim.py [Number of points] [Frequency] [Timestep] [SNR (dB)] [Phase Offset (radians)] [Output Filename]>
+# < python FIDSim.py [Number of points] [Frequency] [Timestep] [Decay Constant (Tau)] [SNR (dB)] [Phase Offset (radians)] [Output Filename]>
 
 # Take inputs:
 NumPoints = int(sys.argv[1])
 Freq = float(sys.argv[2])
 Timestep = float(sys.argv[3])
-SNR = float(sys.argv[4])
-PhaseOffset = float(sys.argv[5])
-OutFile = str(sys.argv[6])
+Decay_Time = float(sys.argv[4])
+SNR = float(sys.argv[5])
+PhaseOffset = float(sys.argv[6])
+OutFile = str(sys.argv[7])
 
 # Calculate the variance from the given SNR value in order to set the width of the Gaussian noise to be added
 Variance = (1.0/(2.0*SNR))
